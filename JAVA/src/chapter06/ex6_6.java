a266ddeb3d79a06ef62c8470496067f1a9412b92
qoqbob <qoqbobb@naver.com>
2022-07-04 22:46:32 +0900

    객체연습

--- JAVA/src/chapter06/ex6_6.java
+++ JAVA/src/chapter06/ex6_6.java
@@ -1,5 +1,20 @@
 package chapter06;
 
+class data {int x;}
+
 public class ex6_6 {
+	public static void main(String[] args) {	
+		Data d = new Data();
+		d.x =10;
+		System.out.println("main() :x "+d.x);
+		
+		chage(d.x);
+		System.out.println("After change(d.x)");
+		
+	}
+static void change(int x) {
+	x = 1000;
+	System.out.println("change():x="+x);
+}
 
 }
